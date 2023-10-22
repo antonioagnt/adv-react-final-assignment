5a19cf9f36f07a619d7fc6e5aa18c3b30131e0c4
antonioagnt <2728086+antonioagnt@users.noreply.github.com>
2023-10-22 14:31:13 -0300

    Finished step 04

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from "react";
-import { useFormik } from "formik";
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
 import {
   Box,
   Button,
@@ -11,73 +11,118 @@ import {
   Select,
   Textarea,
   VStack,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 import * as Yup from 'yup';
-import FullScreenSection from "./FullScreenSection";
-import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import FullScreenSection from './FullScreenSection';
+import useSubmit from '../hooks/useSubmit';
+import { useAlertContext } from '../context/alertContext';
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
+  const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
+  useEffect(() => {
+    if (response) {
+      if (response.type === 'success') {
+        onOpen('success', response.message);
+        formik.resetForm();
+      } else if (response.type === 'error') {
+        onOpen('error', response.message);
+      }
+    }
+  }, [response]);
   const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
+    initialValues: {
+      firstName: '',
+      email: '',
+      type: 'hireMe',
+      comment: '',
+    },
+    onSubmit: (values) => {
+      submit('url', values);
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required('Required'),
+      email: Yup.string().required('Required').email('Invalid email address'),
+      type: Yup.string(),
+      comment: Yup.string()
+        .required('Required')
+        .min(25, 'Must be at least 25 characters'),
+    }),
   });
 
   return (
     <FullScreenSection
       isDarkBackground
-      backgroundColor="#512DA8"
+      backgroundColor='#512DA8'
       py={16}
       spacing={8}
     >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
+      <VStack w='1024px' p={32} alignItems='flex-start'>
+        <Heading as='h1' id='contactme-section'>
           Contact me
         </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form>
+        <Box p={6} rounded='md' w='100%'>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              formik.handleSubmit(e);
+            }}
+          >
             <VStack spacing={4}>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
+              <FormControl
+                isInvalid={formik.errors.firstName && formik.touched.firstName}
+              >
+                <FormLabel htmlFor='firstName'>Name</FormLabel>
                 <Input
-                  id="firstName"
-                  name="firstName"
+                  id='firstName'
+                  name='firstName'
+                  {...formik.getFieldProps('firstName')}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
+              <FormControl
+                isInvalid={formik.errors.email && formik.touched.email}
+              >
+                <FormLabel htmlFor='email'>Email Address</FormLabel>
                 <Input
-                  id="email"
-                  name="email"
-                  type="email"
+                  id='email'
+                  name='email'
+                  type='email'
+                  {...formik.getFieldProps('email')}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
               </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
+              <FormControl
+                isInvalid={formik.errors.type && formik.touched.type}
+              >
+                <FormLabel htmlFor='type'>Type of enquiry</FormLabel>
+                <Select id='type' name='type' {...formik.getFieldProps('type')}>
+                  <option value='hireMe'>Freelance project proposal</option>
+                  <option value='openSource'>
                     Open source consultancy session
                   </option>
-                  <option value="other">Other</option>
+                  <option value='other'>Other</option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
+              <FormControl
+                isInvalid={formik.errors.comment && formik.touched.comment}
+              >
+                <FormLabel htmlFor='comment'>Your message</FormLabel>
                 <Textarea
-                  id="comment"
-                  name="comment"
+                  id='comment'
+                  name='comment'
                   height={250}
+                  {...formik.getFieldProps('comment')}
                 />
-                <FormErrorMessage></FormErrorMessage>
+                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
               </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
+              <Button
+                type='submit'
+                colorScheme='purple'
+                width='full'
+                isLoading={isLoading}
+              >
                 Submit
               </Button>
             </VStack>
